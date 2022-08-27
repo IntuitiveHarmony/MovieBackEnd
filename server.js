2b0b6ad3908a0d849dca17faaa8aa36a4efd12e0
ununique3 <j.horst77@gmail.com>
2022-08-27 15:57:48 -0600

    server needs help

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const db = mongoose.connection;
 require('dotenv').config()
-
+const Movie = require('./models/movie.js');
 const PORT = process.env.PORT || 3003;
 
-const Movie = require('./models/movie.js');
+
 
 const MONGODB_URI  = process.env.MONGODB_URI
 
 const app = express();
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
+
+
+
+
+//-----------------------------------------------
+//      GET ALL MOVIES
+//-----------------------------------------------
+// app.get('/movies', (req, res) => {
+//   Movie.find({}, (err,foundMovies) => {
+//     res.json(foundMovies)
+//   })
+// })
 //-----------------------------------------------
 //      NEW MOVIE
 //-----------------------------------------------
@@ -22,42 +34,32 @@ app.post('/movies', (req, res) => {
     res.json(createdMovie)
   })
 })
-
-//-----------------------------------------------
-//      GET SINGLE MOVIE
-//-----------------------------------------------
-app.get('/movies/:id', (req, res) => {
-  Movie.find({}, (err,foundMovies) => {
-    res.json(foundMovies)
-  })
-})
-
-//-----------------------------------------------
-//      GET ALL MOVIES
-//-----------------------------------------------
-app.get('/movies', (req, res) => {
-  Movie.find({}, (err,foundMovies) => {
-    res.json(foundMovies)
-  })
-})
-
 //-----------------------------------------------
 //      DELETE MOVIE
 //-----------------------------------------------
-app.delete('/movies/:id', (req, res) => {
-  Movie.findByIdAndRemove(req.params.id, (err, deletedMovie) => {
-    res.json(deletedMovie)
-  })
-})
+// app.delete('/movies/:id', (req, res) => {
+//   Movie.findByIdAndRemove(req.params.id, (err, deletedMovie) => {
+//     res.json(deletedMovie)
+//   })
+// })
 
 //-----------------------------------------------
 //      EDIT MOVIE
 //-----------------------------------------------
-app.put('/movies/:id', (req, res) => {
-  Movie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedMovie) => {
-    res.json(updatedMovie)
-  })
-})
+// app.put('/movies/:id', (req, res) => {
+//   Movie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedMovie) => {
+//     res.json(updatedMovie)
+//   })
+// })
+
+//-----------------------------------------------
+//      GET SINGLE MOVIE
+//-----------------------------------------------
+// app.get('/movies/:id', (req, res) => {
+//   Movie.find({}, (err,foundMovies) => {
+//     res.json(foundMovies)
+//   })
+// })
 
 //-----------------------------------------------
 //      CONNECTIONS
@@ -70,6 +72,11 @@ app.listen(3000, () => {
 
 // CONNECT TO ATLAS AND NAME SUBDATABASE
 mongoose.connect(MONGODB_URI)
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
 mongoose.connection.once('open', ()=>{
     console.log('connected to port: ',PORT);
 })
